05000177c8a4609990fa71a388c050cac2399828
Andrew Latham <acl68@case.edu>
2012-03-13 21:13:15 -0400

    The first commit!

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Lyrics Commander</title>
+		<link rel="stylesheet" type="text/css" href="homestyle.css" />
+	<head>
+
+	<body>
+		<div id="titlebar">
+			<span id="titletext">Lyrics Commander</span>
+		</div>
+		<div id="menubar">Some stuff, some more stuff</div>
+		<h1 style="text-align: center">Lyrics Commander</h1>
+		<br />
+		<p>Lyrics Commander will command your lyrics and tell you which songs you like.</p>
+		
+		<div id="centerdiv">
+			<button id="centerbutton" onclick = "window.location.href = 'main.php'">Get Started</button>
+		</div>
+	</body>
+</html>
---
+++ main.php
@@ -0,0 +1,12 @@
+<?php //main.php
+echo <<<_HDOC
+<html>
+	<head>
+		<title>Lyrics Commander</title>
+	<head>
+	<body>
+		<h1>This is the page where things happen</h1>
+	</body>
+</html>
+_HDOC;
+?>
