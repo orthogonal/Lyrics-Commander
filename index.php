a60f0c4cccfda75eb6f9f87a6d54755210a98f6d
Andrew Latham <acl68@case.edu>
2012-03-13 22:47:14 -0400

    Added some stuff to the main page:  A bar on the bottom, a header, and a main table with two cells.

--- index.php
+++ index.php
@@ -8,13 +8,28 @@
 		<div id="titlebar">
 			<span id="titletext">Lyrics Commander</span>
 		</div>
-		<div id="menubar">Some stuff, some more stuff</div>
-		<h1 style="text-align: center">Lyrics Commander</h1>
-		<br />
-		<p>Lyrics Commander will command your lyrics and tell you which songs you like.</p>
 		
-		<div id="centerdiv">
-			<button id="centerbutton" onclick = "window.location.href = 'main.php'">Get Started</button>
+		<div id="maindiv">
+			<table id="centertable">
+				<tr>
+					<td id="leftbox">
+						<button id="centerbutton" onclick = "window.location.href = 'main.php'">Get Started</button>
+					</td>
+					
+					<td id="rightbox">
+						<p>Lyrics Commander will command your lyrics and tell you which songs you like.</p>
+					</td>
+				</tr>
+			</table>
 		</div>
+		
+		<div id="bottombar">
+			<li>
+				<a href="#settings">Settings</a>
+				<a href="#logout">Logout</a>
+				<a href="#about">About</a>
+			</li>
+		</div>
+			
 	</body>
 </html>
