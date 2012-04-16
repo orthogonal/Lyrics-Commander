fc51feb196bfe579a4bb63b40ed05e28bcc1d962
Andrew Latham <acl68@case.edu>
2012-04-16 13:28:59 -0400

    Starting a new index layout

---
+++ index2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Lyrics Commander</title>
+			<link rel="stylesheet" type="text/css" href="homestyle.css" />
+				<script src="_js/jquery-1.7.js"></script>
+	</head>
+	<body>
+		<div id="titlebar">
+			<span id="titletext">Lyrics Commander</span>
+		</div>
+	</body>
+</html>
