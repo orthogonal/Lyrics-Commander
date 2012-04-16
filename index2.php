4af93f0c73986b43c9b85afb0083bff575237450
Andrew Latham <acl68@case.edu>
2012-04-16 14:27:49 -0400

    A very startup-y color scheme and layout.  Flashier logo and title bar.

--- index2.php
+++ index2.php
@@ -2,12 +2,27 @@
 <html>
 	<head>
 		<title>Lyrics Commander</title>
-			<link rel="stylesheet" type="text/css" href="homestyle.css" />
+			<link rel="stylesheet" type="text/css" href="indexstyle.css" />
 				<script src="_js/jquery-1.7.js"></script>
 	</head>
 	<body>
 		<div id="titlebar">
 			<span id="titletext">Lyrics Commander</span>
+		</div>
+		
+		<div id="containerdiv">
+			<div id="centerdiv">
+				<table id="centertable">
+					<tr>
+						<td id="left">
+							Hi
+						</td>
+						<td id="right">
+							No
+						</td>
+					</tr>
+				</table>
+			</div>
 		</div>
 	</body>
 </html>
