7998f5e3a06f41c7c6af9dc7fb66ea270d8cdef9
joshuaschwarz <josh@joshuaschwarz.com>
2012-05-08 11:54:22 -0700

    Deleted Justin folder.

--- Justin/db_login.php
+++
@@ -1,6 +0,0 @@
-<?php  //db_login.php
-$db_hostname = 'lyricscommander.db.8271005.hostedresource.com';
-$db_database = 'lyricscommander';
-$db_username = 'lyricscommander';
-$db_password = 'Meral341';
-?>
