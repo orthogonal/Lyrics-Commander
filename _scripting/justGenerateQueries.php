add74d4c9684973e1d576542c9689bb33fed5abe
joshuaschwarz <josh@joshuaschwarz.com>
2012-05-07 23:24:21 -0700

    Changes to scripts.  More queries.

--- _scripting/justGenerateQueries.php
+++ _scripting/justGenerateQueries.php
@@ -2,11 +2,15 @@
 $key = "b25b959554ed76058ac220b7b2e0a026";
 
 $artists = split("\n", file_get_contents("artists.txt"));
+if(count($artists) == 1) $artists = split("\r\n", file_get_contents("artists.txt"));
 //$artists = array_reverse($artists);
-$artistCounter = 1;
-$albumCounter = 1;
-$trackCounter = 1;
-$stanzaCounter = 1;
+
+$artistCounter = 143;
+$albumCounter = 508;
+$trackCounter = 508;
+$stanzaCounter = 508;
+
+$startAt = "Lil' Wayne";
 
 $testLimit = 1000;//artist limit
 $testTrackLimit = 5;//track limit
@@ -15,15 +19,24 @@ $testStanzaLimit = 1;//stanza limit
 $counter = 0;
 foreach($artists as $artist)
 {
+	if($startAt != "" && $startAt != $artist)
+	{
+		continue;
+	}
+	else
+	{
+		$startAt = "";	
+	}
+	
 	if($artist == "")	continue;
 	//if($counter == 500)
 		//$testTrackLimit = 1; //reduce track limit for more popular artists
 	
 	$artistCopy = str_replace("'", "\\'", $artist);
-	$query = "SELECT ArtistID FROM Artist WHERE Name='$artistCopy' LIMIT 1";
 	$artistID = $artistCounter;
+
+	$xml0 = new SimpleXMLElement(file_get_contents("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" . str_replace("'","%27",str_replace(" ", "%20", $artist)) . "&api_key=$key"));
 	
-	$xml0 = new SimpleXMLElement(file_get_contents("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" . str_replace(" ", "%20", $artistCopy) . "&api_key=$key"));
 	$image = "";
 	//$bio = "";
 	foreach($xml0->artist as $artistTemp)
@@ -42,22 +55,26 @@ foreach($artists as $artist)
 	print "$query<hr>";
 	$artistCounter++;
 	
-	$xml1 = new SimpleXMLElement(file_get_contents("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" . str_replace(" ", "%20", $artist) . "&api_key=$key"));
+	$xml1 = new SimpleXMLElement(file_get_contents("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" . str_replace("'","%27",str_replace(" ", "%20", $artist)) . "&api_key=$key"));
 	$countOfTracks = 0;
 	foreach($xml1->toptracks->track as $track)
 	{
-		$trackName = str_replace("'", "\\'", $track->name);
-		$xml2 = new SimpleXMLElement(file_get_contents("http://ws.audioscrobbler.com/2.0/?method=track.getinfo&api_key=$key&artist=" . str_replace(" ", "%20", $artistCopy) . "&track=" . str_replace(" ", "%20", $trackName)));
+		$trackName = $track->name;
+		$trackNameCopy = str_replace("'", "\\'", $track->name);
+		$xml2 = new SimpleXMLElement(file_get_contents("http://ws.audioscrobbler.com/2.0/?method=track.getinfo&api_key=$key&artist=" .  str_replace("'","%27",str_replace(" ", "%20", $artist)) . "&track=" . str_replace(" ", "%20", $trackName)));
 
 		$albumTitle = null;
+		$albumTitleCopy = null;
 		foreach($xml2->track->album as $album)
 		{
-			$albumTitle = str_replace("'", "\\'", $album->title);
+			$albumTitle = $album->title;
+			$albumTitleCopy = str_replace("'", "\\'", $album->title);
 			break;
 		}
 		if($albumTitle == null)
 		{
 			$albumTitle = "Unknown Album";
+			$albumTitleCopy = "Unknown Album";
 		}
 		$albumID = $albumCounter;
 
@@ -76,7 +93,7 @@ foreach($artists as $artist)
 		}
 		
 		$query =  	"INSERT INTO Album (Name, CoverURL)
-					VALUES ('$albumTitle', '$coverURL')";
+					VALUES ('$albumTitleCopy', '$coverURL')";
 		print "$query<hr>";
 		$albumCounter++;
 		
@@ -84,7 +101,7 @@ foreach($artists as $artist)
 		$trackID = $trackCounter;
 		
 		$query =  	"INSERT INTO Song (AlbumID, ArtistID, Name)
-					VALUES ($albumID, $artistID, '$trackName')";
+					VALUES ($albumID, $artistID, '$trackNameCopy')";
 		print "$query<hr>";
 		$trackCounter++;		
 			
@@ -134,6 +151,7 @@ function urlOfLyrics($artist, $song)
 		$song = str_replace($c,"",$song);
 	
 	if(strpos($song, "the") === 0)	$song = substr($song, 3);
+	if(strpos($artist, "the") === 0)	$artist = substr($artist, 3);
 	return "http://www.azlyrics.com/lyrics/$artist/$song.html";
 }
 function getLyrics($artist,$song)
@@ -145,6 +163,8 @@ function getLyrics($artist,$song)
 	$endOfLyrics = strpos($data, "<!-- end of lyrics -->");
 	$stanzas = explode("<br>\n<br>\n", substr($data, $startOfLyrics, $endOfLyrics - $startOfLyrics));
 	if(count($stanzas) == 1) $stanzas = explode("<br>\r\n<br>\r\n", substr($data, $startOfLyrics, $endOfLyrics - $startOfLyrics));
+	if(count($stanzas) == 1) $stanzas = explode("<br />\n<br />\n", substr($data, $startOfLyrics, $endOfLyrics - $startOfLyrics));
+	if(count($stanzas) == 1) $stanzas = explode("<br />\r\n<br />\r\n", substr($data, $startOfLyrics, $endOfLyrics - $startOfLyrics));
 	return $stanzas;
 }
 
--- _scripting/justRunQueries.php
+++ _scripting/justRunQueries.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../../db_login.php";
+require_once "../db_login.php";
 $db_server = mysql_connect($db_hostname, $db_username, $db_password);
 mysql_select_db($db_database, $db_server); 
 
